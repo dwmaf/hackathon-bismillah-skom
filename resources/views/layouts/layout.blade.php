fc805ddb85efbcc631b2d44cf9d32e3a3ece7645
nana72848e <d1041211005@student.untan.ac.id>
2024-10-25 22:47:00 +0700

    p

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -13,19 +13,20 @@
 <body>
   <nav class="navbar navbar-expand-lg">
     <div class="container d-flex justify-content-between"> <!-- Ganti container-fluid dengan container -->
-        {{-- <a class="navbar-brand" href="/">
-            <img src="{{ url('/img/logo-jombang1.png') }}" alt="Logo"> <!-- Pindahkan gambar ke navbar -->
-        </a> --}}
+        <a class="navbar-brand" href="/">
+            {{-- <img src="{{ url('/img/logo-jombang1.png') }}" alt="Logo"> --}}
+            Bismillah-Skom
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02"> <!-- Ganti justify-content- menjadi justify-content-end -->
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="/">Home</a>
+                    <a class="nav-link " href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="/recycle">About</a>
+                    <a class="nav-link " href="/Trecycle">Recycle</a>
                 </li>
                 
             </ul>
