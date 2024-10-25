b394fd12084a5576abde4c2c3d3d06152d54c036
Satrio0401 <satrioptk960@gmail.com>
2024-10-26 00:44:19 +0700

    p

--- resources/views/recycle.blade.php
+++ resources/views/recycle.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.layout')
-<<<<<<< HEAD
 
 @section('child')
 <div class="d-flex justify-content-center mb-5 mt-5" style="height: 100vh;">
@@ -36,42 +35,4 @@
         </div>
     </form>
 </div>
-=======
-@section('child')
-    <h1 class="text-center mb-5">Isi data barang</h1>
-    <div class="d-flex justify-content-center " style="height: 100vh;">
-        <form action="/submitrecycle" class="container" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="d-flex flex-row gap-3 ">
-                <div class="mb-3 flex-fill">
-                    <label for="merk" class="form-label">Merk</label>
-                    <select class="form-select border border-black mb-3" id="merk" aria-label="Pilih Merk" name="merk_id">
-                        <option selected>Pilih merk barang</option>
-                        @foreach ($merks as $merk)
-                            <option value="{{ $merk->id }}">{{ $merk->nama_merk }}</option>
-                        @endforeach
-                    </select>
-
-                    <label for="input1" class="form-label">Model</label>
-                    <input type="text" class="form-control border border-black mb-3" id="input1"
-                        placeholder="Isi model barang" name="model">
-                    <label for="input1" class="form-label">Kondisi</label>
-                    <input type="text" class="form-control border border-black mb-3" id="input1"
-                        placeholder="Isi kondisi barang" name="kondisi">
-                </div>
-                <div class="mb-3 flex-fill">
-                    <label for="formFile" class="form-label">Foto barang</label>
-                    <input class="form-control mb-3" type="file" id="formFile" name="foto">
-                    <label for="input1" class="form-label">Lokasi</label>
-                    <input type="text" class="form-control border border-black mb-3" id="input1"
-                        placeholder="Isi lokasi outlet" name="lokasi">
-                    <label for="deskripsi" class="form-label">Deskripsi</label>
-                    <textarea class="form-control border border-black mb-3" id="deskripsi" rows="3"
-                        placeholder="isi deskripsi lebih lanjut di sini" name="deskripsi"></textarea>
-                </div>
-            </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
->>>>>>> ec4445ffcbe463599156ba0d3f38fec135bfa5a4
 @endsection
