d912e76f7a599da629695482f34849d7f02a8b17
nana72848e <d1041211005@student.untan.ac.id>
2024-10-25 21:24:04 +0700

    push

---
+++ resources/views/recycle.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>Bismillah Skom</title>
+    </head>
+    <body >
+        <h1 class="text-center font-bold text-3xl">Isi data barang</h1>
+        <form class="flex flex-row" action="">
+            <input type="text">
+        </form>
+    </body>
+</html>
