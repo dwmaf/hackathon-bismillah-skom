de3cc383c759c00bf08856aa3a3abbd3f277747e
nana72848e <d1041211005@student.untan.ac.id>
2024-10-25 23:37:49 +0700

    p

--- database/migrations/2024_10_25_163447_create_merks_table.php
+++ database/migrations/2024_10_25_163447_create_merks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('merks', function (Blueprint $table) {
             $table->id();
-            $table->id();
+            $table->string("nama_merk");
             $table->timestamps();
         });
     }
